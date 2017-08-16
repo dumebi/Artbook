3aa96dfd0007b9d0215c2d3b4e886ac95d1e1a83
Jude Dike <dikejude49@gmail.com>
2017-08-16 17:34:01 +0100

    artbook done

---
+++ Art Book/createVC.swift
@@ -0,0 +1,107 @@
+//
+//  createVC.swift
+//  Art Book
+//
+//  Created by Kornet Project on 15/08/2017.
+//  Copyright © 2017 Kornet Project. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class createVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    var chosenPainting = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imageView.isUserInteractionEnabled = true
+        let gestureRecogniser = UITapGestureRecognizer(target: self, action: #selector(createVC.selectImage))
+        imageView.addGestureRecognizer(gestureRecogniser)
+        
+        if chosenPainting != ""{
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            fetchRequest.predicate = NSPredicate(format: "name = %@", self.chosenPainting)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0{
+                    for result in results as![NSManagedObject]{
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                        }
+                        if let year = result.value(forKey: "year") as? Int {
+                            yearText.text = String(year)
+                        }
+                        if let artist = result.value(forKey: "artist") as? String {
+                            artistText.text = artist
+                        }
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            self.imageView.image = image
+                        }
+                    }
+                }
+            } catch {
+                print("error")
+            }
+        }
+        else{
+            imageView.image = UIImage(named: "tapme.png")
+            nameText.text = ""
+            artistText.text = ""
+            yearText.text = ""
+        }
+        
+    }
+
+    func selectImage(){
+        //selecting image from library
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newArt = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        newArt.setValue(nameText.text, forKey: "name")
+        newArt.setValue(artistText.text, forKey: "artist")
+        
+        if let year = Int(yearText.text!){
+            newArt.setValue(year, forKey: "year")
+        }
+        
+        let data = UIImageJPEGRepresentation(imageView.image!, 0.5)
+        newArt.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("we managed to save it successfully")
+        } catch {
+            print("error")
+        }
+        
+        NotificationCenter.default.post(name:NSNotification.Name(rawValue:"paintingCreated"), object:nil)
+        _ = self.navigationController?.popViewController(animated: true)
+        
+    }
+
+}
